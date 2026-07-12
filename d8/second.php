76537f010d961481a25ec64c91faf7f9f8588953
chouhankratika001-glitch <chouhankratika001@gmail.com>
2026-07-12 21:45:52 +0530

    Add files via upload

---
+++ d8/second.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php echo "this is my age main title" ?></h1>
+        <p>this is my paragraph</p>
+
+        <h2><?php echo "this is my page sub title" ?></h2>
+
+        <! -- <h3><?php phipinfo(); ?></h3>
+
+        <?php
+        $name = rahlu 
+
+        $student = ["rahul","riya","kai"];
+
+
+        echo $student[0];
+        echo "<br>";
+        echo $student[1];
+        echo "<br>"
+        echo $student[count($student)  - 1];
+        
+</body>
+</html>
